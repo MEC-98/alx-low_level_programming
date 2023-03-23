8e86df5edcc26fc47e86d72727d22cb3652dc186
Mostafa El Chahmouti <126358648+MEC-98@users.noreply.github.com>
2023-03-23 22:05:38 +0000

    The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    long n = 612852475143;
+    long i;
+
+    while (n % 2 == 0) { // Check for 2 as a factor
+        n /= 2;
+        i = 2;
+    }
+
+    for (i = 3; i <= sqrt(n); i += 2) { // Check for odd prime factors
+        while (n % i == 0) {
+            n /= i;
+        }
+    }
+
+    if (n > 2) { // The largest factor is greater than the square root of n
+        printf("%ld\n", n);
+    } else { // The largest factor is the last prime factor we found
+        printf("%ld\n", i-2);
+    }
+
+    return 0;
+}
