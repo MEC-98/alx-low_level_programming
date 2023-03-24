1dde0c0fe2d1c8172b58449875de18cdcf836010
Mostafa El Chahmouti <126358648+MEC-98@users.noreply.github.com>
2023-03-24 05:09:42 +0000

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
-#include "math.h"
+#include <math.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {
-    long n = 612852475143;
-    long i;
+	long int i, n, max;
 
-    while (n % 2 == 0) { // Check for 2 as a factor
-        n /= 2;
-        i = 2;
-    }
+	n = 612852475143;
+	max = -1;
 
-    for (i = 3; i <= sqrt(n); i += 2) { // Check for odd prime factors
-        while (n % i == 0) {
-            n /= i;
-        }
-    }
-
-    if (n > 2) { // The largest factor is greater than the square root of n
-        printf("%ld\n", n);
-    } else { // The largest factor is the last prime factor we found
-        printf("%ld\n", i-2);
-    }
-
-    return 0;
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
+	for (i = 3 ; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+		max = n;
+	printf("%ld\n", max);
+	return (0);
 }
