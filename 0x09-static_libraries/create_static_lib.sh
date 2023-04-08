602e5d5473cd07f0bab733406ac9ec5bdd145da7
MEC-98 <elchahmouti.mostafa@ofppt-edu.ma>
2023-04-08 15:46:51 -0700

    update

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-gcc -c *.c
+
+# Compile all .c files in the current directory into .o files
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+
+# Create the static library liball.a from the .o files
 ar -rc liball.a *.o
+
+# Index the symbols in the library
+ranlib liball.a
+
+# Clean up the .o files
+rm -f *.o
