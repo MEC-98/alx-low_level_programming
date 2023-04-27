8012f5031ab60912209d5a1fa33da7818504b692
Mostafa El Chahmouti <126358648+MEC-98@users.noreply.github.com>
2023-04-27 22:02:11 +0000

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+https://github.com/MEC-98/alx-low_level_programming
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,7 +27,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
-int '_strlen'(char *s);
+int _strlen(char *s);
 
 
 #endif
